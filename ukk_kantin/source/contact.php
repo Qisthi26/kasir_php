598f4b6b0d2c035ded6c6bbd3d1bc554b5775355
Qisthi <145886901+Qisthi26@users.noreply.github.com>
2025-06-03 07:02:39 +0700

    Create contact.php

---
+++ ukk_kantin/source/contact.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <title>Kantin Telkom</title>
+
+    <style>
+        body {
+            padding-top: 60px;
+        }
+    </style>
+</head>
+<body>
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg fixed-top bg-light-subtle border-bottom border-danger border-4">
+        <div class="container">
+            <!-- logo -->
+            <a class="navbar-brand" href="#about">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJBBUnkE6bLR4vXkghOR5P3sWRF3V3LjA3ZA&s" alt="logo" width="30">
+            </a>
+            <!-- menu navbar -->
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link"  href="#about">About Kantin</a>
+                    </li>
+                    <li class="nav-item hover">
+                        <a class="nav-link" href="list2.php">Cafeteria List</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="howto.php">How to Buy</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    <h2>Contact Us</h2>
+                </div>
+            </div>
+        <div class="row ">
+            <div class="col-md-8">
+                <form>
+                    <div class="mb-3">
+                        <label for="pesan" class="form-label">Masukan pesan</label>
+                        <input type="pesan" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                        
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+        </div>
+    </section>
